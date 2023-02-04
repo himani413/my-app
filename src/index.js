d2baea6c9e51ffbf3670f8515d6f6f073b6fb816
Himani <pererau-se19042@stu.kln.ac.lk>
2023-02-04 20:55:22 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 
+function AddPersonForm(props) {
+  const [ person, setPerson ] = useState('');
+    
+  function handleChange(e) {
+    setPerson(e.target.value);
+  }
+    
+  function handleSubmit(e) {
+    if(person !== '') {
+      props.handleSubmit(person);
+      setPerson('');
+    }
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" 
+        placeholder="Add new contact" 
+        onChange={handleChange} 
+        value={person} />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1>Hello, React!</h1>
+function PeopleList(props) {
+  const arr = props.data;
+  const listItems = arr.map((val, index) =>
+    <li key={index}>{val}</li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+function ContactManager(props) {
+  const [contacts, setContacts] = useState(props.data);
+
+  function addPerson(name) {
+    setContacts([...contacts, name]);
+  }
+
+  return (
+    <div>
+      <AddPersonForm handleSubmit={addPerson} />
+      <PeopleList data={contacts} />
+    </div>
+  );
+}
+const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
+
+ReactDOM.render(
+  <ContactManager data={contacts} />, 
+  document.getElementById('root')
 );
 
 
